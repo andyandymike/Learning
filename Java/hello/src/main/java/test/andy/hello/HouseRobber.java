f906f15727ecbaeb65a136f70319f92955dfa46a
andyandymike <564458508@qq.com>
2017-08-24 23:40:26 +0800

    home update

--- Java/hello/src/main/java/test/andy/hello/HouseRobber.java
+++ Java/hello/src/main/java/test/andy/hello/HouseRobber.java
@@ -3,11 +3,43 @@ package test.andy.hello;
 public class HouseRobber {
 	
 	private int[] sub;
+	private int end;
 	
 	public long houseRobber(int[] A) {
+		int len = A.length;
+		if(A == null || len == 0) {
+			return 0;
+		}
+		long[] res = new long[len];
+		for(int i = 0; i < len; i++) {
+			if(i == 0) {
+				res[i] = A[i];
+				continue;
+			}
+			if(i == 1) {
+				res[i] = Math.max(A[0], A[1]);
+				continue;
+			}
+			res[i] = Math.max(res[i - 1], res[i - 2] + A[i]);
+		}
+		return res[len - 1];
+		
+    }
+	
+	public long ohouseRobber(int[] A) {
         long max = 0;
         this.sub = A;
-        for (int i = 0; i < A.length; i++) {
+        this.end = A.length - 1;
+        if (A.length == 0) {
+        	return 0;
+        }
+        if (A.length == 1) {
+        	return A[0];
+        }
+        if (A.length == 2) {
+        	return A[0] > A[1]? A[0] : A[1];
+        }
+        for (int i = 0; i < 2; i++) {
         	long cmax = rFindMaxSub(i);
         	System.out.println("index: " + String.valueOf(i) + " value: " + String.valueOf(A[i]) + " max: " + String.valueOf(cmax));
         	if (cmax > max) {
@@ -19,19 +51,20 @@ public class HouseRobber {
 	
 	private long rFindMaxSub(int start) {
 		long max = 0;
-        if (sub.length - start == 1) {
-        	max = sub[sub.length - 1];
+		if (start >= sub.length) {
+			return 0;
+		}
+        if (sub.length == start + 2 || sub.length == start + 1) {
+        	max = sub[start];
         	return max;
         }
-        if (sub.length - start == 2) {
-        	max = sub[sub.length - 1] > sub[sub.length - 2] ? sub[sub.length - 1] : sub[sub.length - 2];
-        	return max; 
-        }
-        for(int i = start; i < sub.length; i++) {
+        long max2 = sub[start] + rFindMaxSub(start + 2);
+        long max3 = sub[start] + rFindMaxSub(start + 3);
+        return max2 > max3? max2: max3;
+        /*for(int i = start; i < sub.length; i++) {
         	long temp = sub[i] + rFindMaxSub(i + 2);
         	max = temp > max? temp : max;
-        }
-        return max;
+        }*/
     }
 
 }
